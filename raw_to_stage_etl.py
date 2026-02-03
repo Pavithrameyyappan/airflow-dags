62a18c4a9317e20eb899b30992b2434b0807985e
Pavithra <>
2026-02-03 12:05:54 +0530

    Update raw_to_stage_etl.py

--- raw_to_stage_etl.py
+++ raw_to_stage_etl.py
@@ -14,18 +14,18 @@ default_args = {
 
 # Define the DAG
 with DAG(
-    dag_id='Raw_To_Stage',
+    dag_id='hello_demo',
     default_args=default_args,
     description='DAG to run a script on a remote worker via Celery queue',
     schedule=None,  # manual trigger only
-    start_date=datetime(2026, 1, 7),
+    start_date=datetime(2026, 3, 2),
     catchup=False,
     tags=['remote_worker'],
 ) as dag:
 
    run_remote_script = BashOperator(
-    task_id='run_remote_script',
-    bash_command='/opt/airflow/venv/py-env/bin/python "/opt/airflow/scripts/LEO%20AWS%20Migration/ETL/S3_Snowpipe_ETL/main_run.py"',
+    task_id='hello_demo',
+    bash_command='/opt/airflow/venv/py-env/bin/python "/opt/airflow/scripts/LEO%20AWS%20Migration/ETL/hello_demo.py"',
     queue='remote_queue',
    )
 
